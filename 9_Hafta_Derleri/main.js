217bac7d7906541c63fd54ee97a489e75d7972f0
PMohamads <116195774+PMohamads@users.noreply.github.com>
2023-06-02 01:05:23 +0300

    DataBase_By_Electron

---
+++ 9_Hafta_Derleri/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, Menu, MenuItem , globalShortcut} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+  })
+
+  win.loadFile('sql_as_table.html')
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+  globalShortcut.register('Alt+Shift+U', () => {
+    console.log('Electron loves global shortcuts!')
+  })
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
